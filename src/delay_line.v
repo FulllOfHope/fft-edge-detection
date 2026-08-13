8c25daf15d585222edea23ffb76b8f5b5ac834e2
Niraj Hariyani <nirajhariyani3214@gmail.com>
2026-08-13 12:51:55 +0530

    Clean up comments in delay_line.v
    Removed header comments from delay_line.v

--- src/delay_line.v
+++ src/delay_line.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.03.2026 02:51:28
-// Design Name: 
-// Module Name: delay_line
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module delay_line #(
     parameter DEPTH = 1

618413e94fd1fd42e0e718dc2030fe2dfc5906a9
Niraj Hariyani <nirajhariyani3214@gmail.com>
2026-08-17 12:31:49 +0530

    Update axi_hpf.v

--- src/axi_hpf.v
+++ src/axi_hpf.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2026 16:37:02
-// Design Name: 
-// Module Name: axi_hpf
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module axi_hpf #(
     parameter N = 1024,
